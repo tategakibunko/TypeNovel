c482f7f11108eeb6a91c4ca1aeb051fad6256dd6
tategakibunko <lambda.watanabe@gmail.com>
2019-07-11 13:15:01 +0900

    Updated version

--- build.fsx
+++ build.fsx
@@ -8,7 +8,7 @@ open Fake.IO
 open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 
-let version = "0.9.0"
+let version = "0.9.1"
 
 Target.initEnvironment()
 Target.create "Clean" <| fun _ ->
