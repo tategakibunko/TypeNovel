2cc85a3dd580950adee6ce36b71650a14b2c3926
tategakibunko <lambda.watanabe@gmail.com>
2019-07-15 13:20:55 +0900

    Remain \u3000 for head space when trimming

--- Lib/StrUtils.fs
+++ Lib/StrUtils.fs
@@ -3,8 +3,34 @@ namespace TypeNovel.Lib
 module StrUtils = begin
   open System.Text.RegularExpressions
 
+  let headTrimChars: char array = [|
+    '\u0009';  // CHARACTER TABULATION
+    '\u000A';  // LINE FEED
+    '\u000B';  // LINE TABULATION
+    '\u000C';  // FORM FEED
+    '\u000D';  // CARRIAGE RETURN
+    '\u0020';  // SPACE
+    '\u00A0';  // NO-BREAK SPACE
+    '\u2000';  // EN QUAD
+    '\u2001';  // EM QUAD
+    '\u2002';  // EN SPACE
+    '\u2003';  // EM SPACE
+    '\u2004';  // THREE-PER-EM SPACE
+    '\u2005';  // FOUR-PER-EM SPACE
+    '\u2006';  // SIX-PER-EM SPACE
+    '\u2007';  // FIGURE SPACE
+    '\u2008';  // PUNCTUATION SPACE
+    '\u2009';  // THIN SPACE
+    '\u200A';  // HAIR SPACE
+    '\u200B';  // ZERO WIDTH SPACE
+    // '\u3000';  // IDEOGRAPHIC SPACE
+    '\uFEFF' // ZERO WIDTH NO-BREAK SPACE
+  |]
+
   let trimString (text: string) =
-    let text = Regex.Replace(text, "^[\n\s]*", "")
+    let text = text.TrimStart(headTrimChars)
+    // \s of dotnet includes '\u3000'!!
+    // let text = Regex.Replace(text, "^[\n\s]*", "")
     let text = Regex.Replace(text, "[\n\s]*$", "")
     text
 
