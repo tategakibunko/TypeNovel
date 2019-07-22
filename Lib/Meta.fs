cf0bf3bf7106b2216f12ca9d721ff62c6c021ef0
tategakibunko <lambda.watanabe@gmail.com>
2019-07-23 08:25:09 +0900

    Modify @tip, and prepare for 0.9.7

--- Lib/Meta.fs
+++ Lib/Meta.fs
@@ -1,7 +1,7 @@
 namespace TypeNovel.Lib
 
 module Meta = begin
-  let Version = "0.9.6"
+  let Version = "0.9.7"
   let ProgramName = "tnc"
   let DefaultConfigFile = "tnconfig.json"
 end
--- build.fsx
+++ build.fsx
@@ -8,7 +8,7 @@ open Fake.IO
 open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 
-let version = "0.9.6"
+let version = "0.9.7"
 
 Target.initEnvironment()
 Target.create "Clean" <| fun _ ->
