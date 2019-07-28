40fd3e6ff48df8fc6eadb10c5d3f071133c7056b
tategakibunko <lambda.watanabe@gmail.com>
2019-07-28 10:28:22 +0900

    Prepare for v1.0.0

--- Lib/Meta.fs
+++ Lib/Meta.fs
@@ -1,7 +1,7 @@
 namespace TypeNovel.Lib
 
 module Meta = begin
-  let Version = "0.9.9"
+  let Version = "1.0.0"
   let ProgramName = "tnc"
   let DefaultConfigFile = "tnconfig.json"
 end
--- build.fsx
+++ build.fsx
@@ -8,7 +8,7 @@ open Fake.IO
 open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 
-let version = "0.9.9"
+let version = "1.0.0"
 
 Target.initEnvironment()
 Target.create "Clean" <| fun _ ->
