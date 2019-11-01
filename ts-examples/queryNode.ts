3693f69b7351d6fb883ad59399bced9eaefb0b80
tategakibunko <lambda.watanabe@gmail.com>
2019-11-01 15:59:53 +0900

    Reshape example

--- ts-examples/queryNode.ts
+++ ts-examples/queryNode.ts
@@ -1,18 +1,38 @@
-import { Compile, NearlyParser, NodeBuilder, TnNode, BlockNode, NodeWhiteSpaceCleaner, DefaultTnConfig } from '../dist';
+import {
+  Compile,
+  NearlyParser,
+  NodeBuilder,
+  TnNode,
+  BlockNode,
+  NodeWhiteSpaceCleaner,
+  DefaultTnConfig,
+} from '../dist';
 
-const topNode = Compile.nodeFromFile('../tn-examples/example.tn', {
-  typeNovelParser: new NearlyParser(),
-  astMappers: [],
-  astConverter: new NodeBuilder(DefaultTnConfig.markupMap || {}),
-  nodeMappers: [new NodeWhiteSpaceCleaner()]
-}) as BlockNode;
+function getNodeFromFile(path: string): BlockNode {
+  return Compile.nodeFromFile(path, {
+    typeNovelParser: new NearlyParser(),
+    astMappers: [],
+    astConverter: new NodeBuilder(DefaultTnConfig.markupMap || {}),
+    nodeMappers: [new NodeWhiteSpaceCleaner()]
+  }) as BlockNode;
+}
 
-const nodes = topNode.queryNode((node: TnNode) => {
-  return (node.isBlockNode() && node.codePos.line <= 38);
-}).sort((n1, n2) => {
-  return n2.codePos.line - n1.codePos.line;
-});
+function getNodeFromLineNo(topNode: BlockNode, lineNo: number): BlockNode | undefined {
+  const nodes = topNode.queryNode((node: TnNode) => {
+    return (node.isBlockNode() && node.codePos.line <= lineNo - 1);
+  }).sort((n1, n2) => {
+    return n2.codePos.line - n1.codePos.line;
+  });
 
-const foundNode = nodes[0] as BlockNode;
-const definedConstraints = foundNode.getConstraints(true);
-console.log(definedConstraints);
+  return (nodes.length > 0) ? nodes[0] as BlockNode : undefined;
+}
+
+const topNode = getNodeFromFile('../tn-examples/example.tn');
+
+for (let line = 1; line <= 50; line++) {
+  const node = getNodeFromLineNo(topNode, line);
+  if (node) {
+    const cntrNames = node.getConstraints(true).map(cntr => cntr.key);
+    console.log(`line: ${line}, block: ${node.name}, cntrs: ${cntrNames}`);
+  }
+}
