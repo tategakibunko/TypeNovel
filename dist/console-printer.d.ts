eef4ff45429d60b36e6fad3db6a6036fdbda9363
tategakibunko <lambda.watanabe@gmail.com>
2021-01-08 23:38:32 +0900

    Removed dist

--- dist/console-printer.d.ts
+++
@@ -1,9 +0,0 @@
-import { ValidationError } from './modules';
-export interface ConsolePrinter {
-    printOutput: (output: string) => void;
-    printValidationError: (error: ValidationError) => void;
-}
-export declare class StdConsolePrinter implements ConsolePrinter {
-    printOutput(output: string): void;
-    printValidationError(error: ValidationError): void;
-}
--- dist/settings.d.ts
+++
@@ -1,3 +0,0 @@
-import { TnConfig } from './modules';
-export declare const DefaultRootBlockName = "body";
-export declare const DefaultTnConfig: TnConfig;
